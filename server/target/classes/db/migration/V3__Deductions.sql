cd4b3be3dbc8c5547f23309a31138d7b0ae95909
Sivasai Podugu <122538291+Sivasai-Podugu@users.noreply.github.com>
2023-09-06 14:07:38 +0530

    Delete server directory

--- server/target/classes/db/migration/V3__Deductions.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE deductions (
-                            `id` INT AUTO_INCREMENT PRIMARY KEY,
-                            `employee_id` INT,
-                            `provident_fund` DECIMAL(10, 2),
-                            `tax` DECIMAL(10, 2),
-                            FOREIGN KEY (`employee_id`) REFERENCES employees(`id`)
-                                ON DELETE CASCADE
-                                ON UPDATE CASCADE
-);

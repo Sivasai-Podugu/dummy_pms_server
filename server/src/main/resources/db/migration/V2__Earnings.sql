6273df53b54e5a301ff2df927dfd3b5d0effd07a
Sivasai Podugu <122538291+Sivasai-Podugu@users.noreply.github.com>
2023-09-06 16:36:48 +0530

    Delete server directory

--- server/src/main/resources/db/migration/V2__Earnings.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE earnings (
-                          `id` INT AUTO_INCREMENT PRIMARY KEY,
-                          `employee_id` INT,
-                          any_allowances DECIMAL(10, 2),
-                          bonus DECIMAL(10, 2),
-                          FOREIGN KEY (`employee_id`) REFERENCES employees(`id`)
-                              ON DELETE CASCADE
-                              ON UPDATE CASCADE
-);

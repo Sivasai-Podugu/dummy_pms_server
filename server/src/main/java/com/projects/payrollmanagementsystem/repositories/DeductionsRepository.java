f652ebcd7cc2bcf327c33d6d38d30a8711394c35
Sivasai Podugu <122538291+Sivasai-Podugu@users.noreply.github.com>
2023-09-07 12:32:48 +0530

    Delete server directory

--- server/src/main/java/com/projects/payrollmanagementsystem/repositories/DeductionsRepository.java
+++
@@ -1,9 +0,0 @@
-package com.projects.payrollmanagementsystem.repositories;
-
-import com.projects.payrollmanagementsystem.models.Deduction;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.Optional;
-
-public interface DeductionsRepository extends JpaRepository<Deduction, Integer> {
-    Optional<Deduction> findByEmployeeId(Integer employeeId);
-}
